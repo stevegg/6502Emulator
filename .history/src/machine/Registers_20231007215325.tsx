273d8905c2b1eb04e41bdf713f41c478814f23cd
sgoyette <steve@goyettefamily.com>
2023-10-07 22:23:07 -0700

    chore: more functionality

--- .history/src/machine/Registers_20231007215325.tsx
+++
@@ -1,9 +0,0 @@
-import { FC, ReactElement, useContext } from 'react';
-import MachineContext from '../MachineContext';
-
-const Registers:FC = ():ReactElement => {
-    const machineContext = useContext(MachineContext);
-
-}
-
-export default Registers;
